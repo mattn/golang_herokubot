812b3166273277f3c5f2392e9356474c73dfe5fc
mattn <mattn.jp@gmail.com>
2013-04-23 19:45:05 +0900

    Separate function

--- web.go
+++ web.go
@@ -99,6 +99,58 @@ func (f Fortune) Write(w io.Writer) error {
 	return err
 }
 
+func handleEvents(events []Event) string {
+	results := ""
+	for _, event := range events {
+		tokens := strings.SplitN(event.Message.Text, " ", 2)
+		if len(tokens) == 1 && tokens[0] == "!heroku" {
+			results += "へろくー"
+		} else if len(tokens) == 2 && tokens[0] == "!weather" {
+			url := fmt.Sprintf("http://openweathermap.org/data/2.1/find/name?q=%s", tokens[1])
+			if res, err := http.Get(url); err == nil {
+				defer res.Body.Close()
+				if res.StatusCode == 200 {
+					var weather Weather
+					if json.NewDecoder(res.Body).Decode(&weather) == nil {
+						icon := weather.List[0].Weather[0].Icon
+						results += fmt.Sprintf("http://openweathermap.org/img/w/%s.png", icon) + "\n"
+					}
+				}
+			}
+		} else if len(tokens) == 2 && tokens[0] == "!uranai" {
+			key := time.Now().Format("2006/01/02")
+			url := fmt.Sprintf("http://api.jugemkey.jp/api/horoscope/free/%s", key)
+			if res, err := http.Get(url); err == nil {
+				defer res.Body.Close()
+				if res.StatusCode == 200 {
+					horoscope := make(map[string]interface{})
+					decoder := json.NewDecoder(res.Body)
+					err := decoder.Decode(&horoscope)
+					if err != nil {
+						results += "ぬっこわれたー"
+					} else {
+						data := horoscope["horoscope"].(map[string]interface{})
+						for _, d := range data[key].([]interface{}) {
+							f := Fortune(d.(map[string]interface{}))
+							if f.IsSign(tokens[1]) {
+								var text bytes.Buffer
+								err = f.Write(&text)
+								if err != nil {
+									results += "ぬっこわれたー"
+								}
+								results += text.String()
+								break
+							}
+						}
+					}
+				}
+			}
+
+		}
+	}
+	return results
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
@@ -108,54 +160,7 @@ func main() {
 				http.Error(w, "bad request", http.StatusBadRequest)
 				return
 			}
-			results := ""
-			for _, event := range status.Events {
-				tokens := strings.SplitN(event.Message.Text, " ", 2)
-				if len(tokens) == 1 && tokens[0] == "!heroku" {
-					results += "へろくー"
-				} else if len(tokens) == 2 && tokens[0] == "!weather" {
-					url := fmt.Sprintf("http://openweathermap.org/data/2.1/find/name?q=%s", tokens[1])
-					if res, err := http.Get(url); err == nil {
-						defer res.Body.Close()
-						if res.StatusCode == 200 {
-							var weather Weather
-							if json.NewDecoder(res.Body).Decode(&weather) == nil {
-								icon := weather.List[0].Weather[0].Icon
-								results += fmt.Sprintf("http://openweathermap.org/img/w/%s.png", icon) + "\n"
-							}
-						}
-					}
-				} else if len(tokens) == 2 && tokens[0] == "!uranai" {
-					key := time.Now().Format("2006/01/02")
-					url := fmt.Sprintf("http://api.jugemkey.jp/api/horoscope/free/%s", key)
-					if res, err := http.Get(url); err == nil {
-						defer res.Body.Close()
-						if res.StatusCode == 200 {
-							horoscope := make(map[string]interface{})
-							decoder := json.NewDecoder(res.Body)
-							err := decoder.Decode(&horoscope)
-							if err != nil {
-								results += "ぬっこわれたー"
-							} else {
-								data := horoscope["horoscope"].(map[string]interface{})
-								for _, d := range data[key].([]interface{}) {
-									f := Fortune(d.(map[string]interface{}))
-									if f.IsSign(tokens[1]) {
-										var text bytes.Buffer
-										err = f.Write(&text)
-										if err != nil {
-											results += "ぬっこわれたー"
-										}
-										results += text.String()
-										break
-									}
-								}
-							}
-						}
-					}
-
-				}
-			}
+			results := handleEvents(status.Events)
 			if len(results) > 0 {
 				results = strings.TrimRight(results, "\n")
 				if runes := []rune(results); len(runes) > 1000 {
